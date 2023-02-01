b532f4cc6dcd3874b46030be9cf3d36d98dda3b7
Fonda Njiemessa Jalil Gafar <fondajalil@hotmail.com>
2023-02-01 05:26:39 +0100

    utilisation des behavior-subject pour afficher une liste d'elt et un elt en particulier

--- src/app/reactive-state/components/single-candidat/single-candidat.component.ts
+++ src/app/reactive-state/components/single-candidat/single-candidat.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Candidate } from '../../models/candidate.model';
+import { CandidatesService } from '../../services/candidates.service';
 
 @Component({
   selector: 'app-single-candidat',
@@ -8,9 +12,31 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class SingleCandidatComponent implements OnInit {
 
-  constructor() { }
+  loading$!: Observable<boolean>;
+  candidate$!: Observable <Candidate>;
+
+  constructor(private candidatesService : CandidatesService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initObservables ()
   }
+  
+  initObservables() {
+    this.loading$ = this.candidatesService.loading$;
+    this.candidate$ = this.route.params.pipe(
+      switchMap(params => this.candidatesService.getCandidateById(+params['id']))
+    );
+  }
+
+  
+  onHire(){};
+
+  onRefuse(){};
+
+  onGoBack(){
+    this.router.navigateByUrl('/reactive-state/candidates')
+  };
 
 }
